9fd06aef5056e793b10804c5873f51300b034a37
kuropAqn <kuropan0819@gmail.com>
2025-02-13 10:31:51 +0000

    1

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,13 @@
+class HomesController < ApplicationController
+  def top
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  #get 'homes/new'
+  #get 'homes/show'
+  #get 'homes/edit'
+  #get 'books/top'
+  get 'books/new'
+  get 'books/show'
+  get 'books/edit'
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
